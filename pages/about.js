42c7baeef0ad01df09f9f0a3c8ac70dbb434f138
Lauro Araujo <lauro.a@myplanet.com>
2020-05-09 13:24:15 -0400

    Added new pages

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,27 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-const Home = () => (
+const About = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>About SSG - NextJS</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        About SSG with <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        This is a new page to test how NextJS's SSG handles navigation.
       </p>
 
       <div className="grid">
-        <Link href='/about'>
+        <Link href='/'>
           <a className="card">
-            <h3>About</h3>
-            <p>About SSG</p>
+            <h3>Home</h3>
+            <p>Go back to the Home Page</p>
           </a>
         </Link>
 
@@ -31,34 +31,6 @@ const Home = () => (
             <p>Navigate to a dynamic route!</p>
           </a>
         </Link>
-
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
 
@@ -215,4 +187,4 @@ const Home = () => (
   </div>
 )
 
-export default Home
+export default About
